100a31d5be7ce9efc772ae5077332d894936edb0
Lagumir <Legals92@mail.ru>
2020-09-22 00:05:11 +0300

    конец 2 лабы

--- lab2/lab3/lab3/lab3.cpp
+++ lab2/lab3/lab3/lab3.cpp
@@ -12,89 +12,119 @@
 
 void ShellNumber(int arr, int randomNumber[]);
 void qs(int randomNumber[], int left, int right);
+void easy(int randomNumber[], int arr);
 
 int main()
 {
+
+  setlocale(LC_ALL, "Rus");
   setvbuf(stdin, NULL, _IONBF, 0);
   setvbuf(stdin, NULL, _IONBF, 0);
 
   srand(time(NULL));
 
+  system("chcp 1251");
+  system("cls");
+
   int *randomNumber;
-  int *higthNumber;
-  int *smallNumber;
-  int *HSNumber;
+
   int arr;
 
   clock_t start, end;
   clock_t start1, end1;
+  clock_t start2, end2;
 
   printf("Размер массива:");
   scanf("%d", &arr);
 
   //Выделяем память
   randomNumber = (int*)malloc(arr * sizeof(int));
-  higthNumber = (int*)malloc(arr * sizeof(int));
-  smallNumber = (int*)malloc(arr * sizeof(int));
-  HSNumber = (int*)malloc(arr * sizeof(int));
 
+
+  FILE *file = fopen("text.txt", "a");
+
+  fprintf(file, "\nВыведем время выполнения программы массив с рандомными числами\n\n");
   //Массив с рандомными числами
-  printf("\nВводим массив с рандомными числами\n");
-  for(int i=0; i<arr; i++){
-    printf("%d\n", randomNumber[i] = rand() %100);
+  printf("\n\n Вводим возростающий за тем убывающий массив\n");
+  for(int i = 1; i<arr/2; i++){
+        randomNumber[i]=i;
+      printf("%d\n",  randomNumber[i]);
+  }
+  for(int i = arr/2; i>0; i--){
+        randomNumber[i]=i;
+      printf("%d\n",  randomNumber[i]);
   }
   start = clock();
 
   ShellNumber(arr, randomNumber);
 
   printf("Сортировка массива :\n\n");
-    for (int i = 0; i < arr; i++)
+  printf("Метод Шерли:\n ");
+    for (int i = 0; i < arr; i++){
         printf("%d ", randomNumber[i]);
+        fprintf(file, "%d ", randomNumber[i]);
+    }
 
   end = clock();
 
-  printf("\n\nВыведем время выполнения программы %lf\n", (double)(end - start)/CLOCKS_PER_SEC);
+  printf("\n\nВыведем время выполнения программы %lf\n\n", (double)(end - start)/CLOCKS_PER_SEC);
+  fprintf(file, "\n\nВыведем время выполнения программы %lf\n\n", (double)(end - start)/CLOCKS_PER_SEC);
 
   start1 = clock();
-  
+
   qs(randomNumber,  0,  arr-1);
-  for (int i = 0; i < arr; i++)
+  printf("Метод qs: \n ");
+  for (int i = 0; i < arr; i++){
       printf("%d ", randomNumber[i]);
+    fprintf(file, "%d ", randomNumber[i]);
+  }
 
   end1 = clock();
-  printf("\n\nВыведем время выполнения программы %lf\n", (double)(end1 - start1)/CLOCKS_PER_SEC);
+  printf("\n\nВыведем время выполнения программы %lf\n\n", (double)(end1 - start1)/CLOCKS_PER_SEC);
+    fprintf(file, "\n\nВыведем время выполнения программы %lf\n\n", (double)(end1 - start1)/CLOCKS_PER_SEC);
+  start2 = clock();
 
-  //Возростающая последовательность
-/*  printf("\n\nВводим возрастающую последовательнусть \n");
+  easy(randomNumber, arr);
+  printf("Быстрая сортировка:\n");
+  for(int i =0; i<arr;i++){
+    printf("%d ", randomNumber[i]);
+      fprintf(file,"%d ", randomNumber[i]);
+  }
+
+  end2 = clock();
+  printf("\n\nВыведем время выполнения программы %lf\n\n", (double)(end2 - start2)/CLOCKS_PER_SEC);
+  fprintf(file, "\n\nВыведем время выполнения программы %lf\n\n", (double)(end2 - start2)/CLOCKS_PER_SEC);
+
+  fprintf(file, "------------------------------------------------------------------------------------------------------------------------------------------------\n");
+
+
+
+/*
+printf("\n\nВводим возрастающую последовательнусть \n");
   for(int i=1; i<=arr; i++){
-    higthNumber[i]=i;
-    printf("%d\n", higthNumber[i]);
-  }
-  //Вводим убывающую последоватьльность
-  printf("\n\n Вводим убывающую последовательность\n");
-
-  for(int i=arr;i>0;i--){
-    smallNumber[i]=i;
-    printf("%d\n", smallNumber[i] );
+    randomNumber[i]=i;
+    printf("%d\n",  randomNumber[i]);
   }
 
-  // Вводим сначала возростающую за тем убывающую последовательность
-  printf("\n\n Вводим возростающий за тем убывающий массив\n");
-  for(int i = 1; i<arr/2; i++){
-      HSNumber[i]=i;
-      printf("%d\n", HSNumber[i]);
-  }
-  for(int i = arr/2; i>0; i--){
-      HSNumber[i]=i;
-      printf("%d\n", HSNumber[i]);
+printf("\nВводим массив с рандомными числами\n");
+    for(int i=0; i<arr; i++){
+      printf("%d\n", randomNumber[i] = rand() %100);
+    }
+
+    printf("\n\n Вводим убывающую последовательность\n");
+
+    for(int i=arr;i>0;i--){
+        randomNumber[i]=i;
+      printf("%d\n",  randomNumber[i] );
+    }
+
+\
+
   }*/
 
 
-
+fclose(file);
   _getch();
-  free(HSNumber);
-  free(smallNumber);
-  free(higthNumber);
   free(randomNumber);
   return 0;
 }
@@ -146,4 +176,19 @@ void qs(int randomNumber[], int left, int right) //вызов функции: qs
 
   if(left < j) qs(randomNumber, left, j);
   if(i < right) qs(randomNumber, i, right);
+}
+
+void easy(int randomNumber[], int arr){
+  for(int i = 0 ; i < arr - 1; i++) {
+       // сравниваем два соседних элемента.
+       for(int j = 0 ; j < arr - i - 1 ; j++) {
+           if(randomNumber[j] > randomNumber[j+1]) {
+              // если они идут в неправильном порядке, то
+              //  меняем их местами.
+              int tmp = randomNumber[j];
+              randomNumber[j] = randomNumber[j+1] ;
+              randomNumber[j+1] = tmp;
+           }
+        }
+    }
 }
